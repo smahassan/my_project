0facadb83e56ff56785ed64a08f12288665d8045
smahassan <tai.suleiman@gmail.com>
2021-01-28 14:19:29 +0100

    Changes to forloop.java

--- Forloop.java
+++ Forloop.java
@@ -34,9 +34,9 @@ public class Forloop {
     }
 
         public static void main (String[] args){
-           // System.out.println(showNumberTables(20));
+           System.out.println(showNumberTables(20));
             //System.out.println(showTimesTables(5, 7));
-            System.out.println(ArithProgression());
+            //System.out.println(ArithProgression());
         }
 
 }
